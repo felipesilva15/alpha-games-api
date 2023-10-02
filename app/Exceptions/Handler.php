e8b5225f2773c6ff73e9c77abeb695a71bbafa98
Felipe Silva <felipe.allware@gmail.com>
2023-10-01 22:58:24 -0300

    Feat: Implementando o ExceptionHandler a aplicação para que os retornos para a API sejam formatados

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -26,5 +27,13 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $e) {
+        if($e instanceof HttpException){
+            return response()->json(["message" => $e->getMessage()], $e->getStatusCode());
+        }
+
+        return parent::render($request, $e);
     }
 }
