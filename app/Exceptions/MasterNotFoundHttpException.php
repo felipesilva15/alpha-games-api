838a2298cbac7f07f245b00261fface009bc7c93
Felipe Silva <felipe.allware@gmail.com>
2023-10-18 20:57:58 -0300

    Fix: Corrigindo a mensagem da exception de registro não encontrado

--- app/Exceptions/MasterNotFoundHttpException.php
+++ app/Exceptions/MasterNotFoundHttpException.php
@@ -6,7 +6,7 @@ use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class MasterNotFoundHttpException extends HttpException
 {
-    public function __construct(string $message = 'Record not found.', \Throwable $previous = null, int $code = 0, array $headers = []) {
+    public function __construct(string $message = 'Registro não encontrado.', \Throwable $previous = null, int $code = 0, array $headers = []) {
         parent::__construct(404, $message, $previous, $headers, $code);
     }
 }
