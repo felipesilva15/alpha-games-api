5c3fdf11ecc40db695cdc11cc14519fd46b6f29c
felipe.silva <felipe.silva@pwi.com.br>
2023-11-23 14:14:22 -0300

    refactor: Refatorando o código de consulta de todos os produtos com o query builder

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -9,54 +9,27 @@ use App\Models\Category;
 class ProductController extends Controller
 {
     public function index(){
-        // Conditions / Where
-        $conditions = [
-            ['PRODUTO_ATIVO', '1']
-        ];
+        $builder = Product::query();
+
+        // Only active
+        $builder->where('PRODUTO_ATIVO', '1');
 
         // Product name
         if(request('search')){
-            array_push($conditions, ['PRODUTO_NOME', 'like', '%'.request('search').'%']);
+            $builder->where('PRODUTO_NOME', 'like', '%'.request('search').'%');
         }
 
         // Price
         if(request('price')){
-            array_push($conditions, ['PRODUTO_PRECO', '<=', request('price')]);
+            $builder->where('PRODUTO_PRECO', '<=', request('price'));
         }
 
+        // Categories
         if(request('categories')){
-            $categories = Category::whereIn('CATEGORIA_ID', request('categories'))->get();
+            $builder->whereIn('CATEGORIA_ID', request('categories'));
         }
 
-        // if($categories){
-        //     $products = Product::where($conditions)
-        //                         ->addSelect(['CATEGORIA_NOME' => Category::select('CATEGORIA_NOME')
-        //                             ->whereColumn('CATEGORIA.CATEGORIA_ID', 'PRODUTO.CATEGORIA_ID')
-        //                             ->orderByDesc('CATEGORIA_NOME')
-        //                             ->limit(1)
-        //                         ])
-        //                         ->whereIn('CATEGORIA_ID', request('categories'))
-        //                         ->withSum('OrderItems', 'item_qtd')
-        //                         ->withSum('ProductStock', 'produto_qtd')
-        //                         ->orderByRaw('case when `product_stock_sum_produto_qtd` > 0 then 1 else 0 end DESC')
-        //                         ->orderByRaw($sortOption)
-        //                         ->paginate($per_page);
-        // } else{
-        //     $products = Product::where($conditions)
-        //                         ->addSelect(['CATEGORIA_NOME' => Category::select('CATEGORIA_NOME')
-        //                             ->whereColumn('CATEGORIA.CATEGORIA_ID', 'PRODUTO.CATEGORIA_ID')
-        //                             ->orderByDesc('CATEGORIA_NOME')
-        //                             ->limit(1)
-        //                         ])
-        //                         ->withSum('OrderItems', 'item_qtd')
-        //                         ->withSum('ProductStock', 'produto_qtd')
-        //                         ->orderByRaw('case when `product_stock_sum_produto_qtd` > 0 then 1 else 0 end DESC')
-        //                         ->orderByRaw($sortOption)
-        //                         ->paginate($per_page);
-        // }
-
-        $data = Product::where($conditions)
-                        ->with("category")
+        $data = $builder->with("category")
                         ->with('images')
                         ->withSum('stock as PRODUTO_QTD', 'produto_qtd')
                         ->get();
@@ -67,7 +40,7 @@ class ProductController extends Controller
     public function show(int $id){
         $data = Product::with("category")
                         ->with('images')
-                        ->withSum('stock as PRODUTO_QTD', 'produto_qtd') // Quantida
+                        ->withSum('stock as PRODUTO_QTD', 'produto_qtd')
                         ->find($id);
 
         if (!$data) {
