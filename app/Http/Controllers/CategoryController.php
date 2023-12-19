a493eec09174679eb89f46de1c6bd71444c283ce
Felipe Silva <felipe.allware@gmail.com>
2023-12-19 20:21:31 -0300

    docs: Finalizando a documentação do controller de categoria do produto

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -11,4 +11,185 @@ class CategoryController extends Controller
         $this->model = $model;
         $this->request = $request;
     }
+
+    /**
+     * @OA\Get(
+     *      path="/api/category",
+     *      tags={"Category"},
+     *      summary="List all categories",
+     *      @OA\Parameter(
+     *         name="CATEGORIA_NOME",
+     *         in="query",
+     *         description="Category name",
+     *         @OA\Schema(type="string")
+     *      ),
+     *      @OA\Parameter(
+     *         name="CATEGORIA_DESC",
+     *         in="query",
+     *         description="Category description",
+     *         @OA\Schema(type="string")
+     *      ),
+     *      @OA\Parameter(
+     *         name="CATEGORIA_ATIVO",
+     *         in="query",
+     *         description="Category active",
+     *         @OA\Schema(
+     *             type="integer",
+     *             enum={1, 0}
+     *         )
+     *      ),
+     *      @OA\Response(
+     *          response="200", 
+     *          description="Category list",
+     *          @OA\JsonContent(
+     *             type="array",
+     *             @OA\Items(ref="#/components/schemas/Category")
+     *         )
+     *      ),
+     *      @OA\Response(
+     *          response="401", 
+     *          description="Unauthorized",
+     *          @OA\JsonContent(ref="#/components/schemas/ApiError")
+     *      )
+     * )
+     */
+    public function index() {
+        return parent::index();
+    }
+
+    /**
+     * @OA\Get(
+     *      path="/api/category/{id}",
+     *      tags={"Category"},
+     *      summary="List an category by ID",
+     *      @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         required=true,
+     *         description="Category ID",
+     *         @OA\Schema(type="integer")
+     *      ),
+     *      @OA\Response(
+     *          response="200", 
+     *          description="Category data",
+     *          @OA\JsonContent(ref="#/components/schemas/Category")
+     *      ),
+     *      @OA\Response(
+     *          response="401", 
+     *          description="Unauthorized",
+     *          @OA\JsonContent(ref="#/components/schemas/ApiError")
+     *      ),
+     *      @OA\Response(
+     *          response="404", 
+     *          description="Record not found",
+     *          @OA\JsonContent(ref="#/components/schemas/ApiError")
+     *      )
+     * )
+     */
+    public function show($id) {
+        return parent::show($id);
+    }
+
+    /**
+     * @OA\Post(
+     *      path="/api/category",
+     *      tags={"Category"},
+     *      summary="Registers an category",
+     *      @OA\RequestBody(
+     *         required=true,
+     *         description="Data for creating a new category",
+     *         @OA\JsonContent(ref="#/components/schemas/Category")
+     *      ),
+     *      @OA\Response(
+     *          response="201", 
+     *          description="Registered category data",
+     *          @OA\JsonContent(ref="#/components/schemas/Category")
+     *      ),
+     *      @OA\Response(
+     *          response="401", 
+     *          description="Unauthorized",
+     *          @OA\JsonContent(ref="#/components/schemas/ApiError")
+     *      ),
+     *      security={{"bearerAuth":{}}}
+     * )
+     */
+    public function store(Request $request) {
+        return parent::store($request);
+    }
+
+    /**
+     * @OA\Put(
+     *      path="/api/category/{id}",
+     *      tags={"Category"},
+     *      summary="Update an category",
+     *      @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         required=true,
+     *         description="Category ID",
+     *         @OA\Schema(type="integer")
+     *      ),
+     *      @OA\RequestBody(
+     *         required=true,
+     *         description="Data for update category",
+     *         @OA\JsonContent(ref="#/components/schemas/Category")
+     *      ),
+     *      @OA\Response(
+     *          response="200", 
+     *          description="Updated category data",
+     *          @OA\JsonContent(ref="#/components/schemas/Category")
+     *      ),
+     *      @OA\Response(
+     *          response="401", 
+     *          description="Unauthorized",
+     *          @OA\JsonContent(ref="#/components/schemas/ApiError")
+     *      ),
+     *      @OA\Response(
+     *          response="404", 
+     *          description="RRecord not found",
+     *          @OA\JsonContent(ref="#/components/schemas/ApiError")
+     *      ),
+     *      security={{"bearerAuth":{}}}
+     * )
+     */
+    public function update(Request $request, $id) {
+        return parent::update($request, $id);
+    }
+
+    /**
+     * @OA\Delete(
+     *      path="/api/category/{id}",
+     *      tags={"Category"},
+     *      summary="Deletes an category",
+     *      @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         required=true,
+     *         description="Category ID",
+     *         @OA\Schema(type="integer")
+     *      ),
+     *      @OA\Response(
+     *          response="200", 
+     *          description="Return message",
+     *          @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(property="message", type="string", example="Registro deletado com sucesso!")
+     *         )
+     *      ),
+     *      @OA\Response(
+     *          response="401", 
+     *          description="Unauthorized",
+     *          @OA\JsonContent(ref="#/components/schemas/ApiError")
+     *      ),
+     *      @OA\Response(
+     *          response="404", 
+     *          description="Record not found",
+     *          @OA\JsonContent(ref="#/components/schemas/ApiError")
+     *      ),
+     *      security={{"bearerAuth":{}}}
+     * )
+     */
+    public function destroy( $id) {
+        return parent::destroy($id);
+    }
 }
